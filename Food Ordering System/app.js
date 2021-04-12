bfa375a59ef01f2428cd9b181543dd44f84eefec
Andreea Gherman <andreeagherman1320@gmail.com>
2021-04-12 10:40:21 +0300

    Second commit for code review

---
+++ Food Ordering System/app.js
@@ -0,0 +1,46 @@
+const keyNameList = document.getElementsByClassName("foodName");
+const priceList = document.getElementsByClassName("price");
+const buttonList = document.getElementsByClassName("button");
+
+const noOfPortions = new Array(12);
+for (let i = 0; i < noOfPortions.length; i++) {
+    noOfPortions[i] = 0;
+}
+
+function cartTotal() {
+    let total = 0;
+    for (let i = 0; i < noOfPortions.length; i++) {
+        total += noOfPortions[i] * parseInt(priceList[i].textContent.replace("$", ""));
+    }
+    return total;
+
+}
+let element = document.getElementById("ShoppingCart");
+function appendParagraph() {
+    element.innerText = "Total: $" + cartTotal();
+}
+
+
+function AddButton(i) {
+    let price = priceList[i].textContent;
+    let name = keyNameList[i].textContent;
+    localStorage.setItem(name, price);
+    noOfPortions[i]++;
+    window.alert("You just added to the cart one portion of " + name + " (" + price + ")." + "\r\n" + " Your total is: $" + cartTotal());
+    let el = document.getElementById('list');
+    let node = document.createElement("li");
+    node.innerText = name + " " + price;
+    el.appendChild(node);
+    appendParagraph();
+
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
